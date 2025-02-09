7c3292b07a4e9ad15c9dfc07bc2a35203d648844
vvspipes99 <103182124+vvspipes99@users.noreply.github.com>
2025-02-10 01:31:53 +0700

    Update setup_prover.sh

--- setup_prover.sh
+++ setup_prover.sh
@@ -9,16 +9,16 @@ fi
 CLAIM_REWARD_ADDRESS=$1
 
 # 第一段命令：删除旧的cysic-prover目录，创建新的目录，并下载必要的文件
-rm -rf ~/cysic-prover
+rm -rf ~/cysic-prover2
 cd ~
-mkdir cysic-prover
+mkdir cysic-prover2
 curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/monitor_linux > ~/cysic-prover/prover
 curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/libdarwin_prover.so  > ~/cysic-prover/libdarwin_prover.so
 curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/libzkp.so  > ~/cysic-prover/libzkp.so
 curl -L https://github.com/cysic-labs/phase2_libs/releases/download/v1.0.0/libcysnet_monitor.so  > ~/cysic-prover/libcysnet_monitor.so
 
 # 第二段命令：创建配置文件
-cat <<EOF > ~/cysic-prover/config.yaml
+cat <<EOF > ~/cysic-prover2/config.yaml
 # Not Change
 chain:
   # Not Change
@@ -45,9 +45,9 @@ server:
 EOF
 
 # 第三段命令：设置执行权限并启动verifier
-cd ~/cysic-prover/
-chmod +x ~/cysic-prover/prover
-echo "LD_LIBRARY_PATH=. CHAIN_ID=534352 ./prover" > ~/cysic-prover/start.sh
-chmod +x ~/cysic-prover/start.sh
+cd ~/cysic-prover2/
+chmod +x ~/cysic-prover2/prover
+echo "LD_LIBRARY_PATH=. CHAIN_ID=534352 ./prover" > ~/cysic-prover2/start.sh
+chmod +x ~/cysic-prover2/start.sh
 
 echo "Cysic prover setup is complete. Run ./start.sh to start the prover."
